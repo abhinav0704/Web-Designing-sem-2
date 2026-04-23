a1eb1240be8f9bc8ad3588b2bba35a7bf66cff7f
Abhinav <abhinav.25b01010774@abes.ac.in>
2026-04-23 12:12:14 +0530

    exp

---
+++ EXP-7/first project/src/props.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function props() {
+  return (
+    <div>
+    <h1>name</h1>
+    <h1>class</h1>
+    <h1>roll no.</h1>
+      </>
+    </div>
+  )
+}
+
+export default props
